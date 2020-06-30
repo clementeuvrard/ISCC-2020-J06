76722f633c5ccb0a78508af470a2ce3dbeffb323
clem1213 <clemeuvrard@gmail.com>
2020-06-30 17:16:49 +0200

    Update chaine.php

--- chaine.php
+++ chaine.php
@@ -1,6 +1,11 @@
 <?php
 
 $strl= "La maîtrise desfondamentaux du code et de la programmation dans les domaines du business, du marketing et de la communication est un avantage sérieux et utile.";
-strlen ("$strl");
 
+echo "<p>la chaine $strl contient" . strlen($strl) . "caractère </p>";
+echo "<p>la chaine $strl contient" . str_world_count($strl) . "mots </p>";
+echo "<p>" . strlower ($strl) . "</p>";
+echo "<p>" . strloupper ($strl) . "</p>";
+echo "<p>" . str_replace ("la" ,"La",$strl) . "</p>";
+echo "<p>" . preg_replace ("est un avantage serieux et utile", $strl) . "</p>"
 ?>
